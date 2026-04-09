4816b39c5970c6f7f767e40ee064757b07796f43
TelloCS <jeremytello31@gmail.com>
2026-04-09 14:08:32 -0500

    fixing deploy

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -3,9 +3,6 @@ set -e
 
 python manage.py wait_for_db
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
-
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
